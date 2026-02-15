3e446bbfa658e1f136ff76d03be55e8a8a1d4e87
gargeyaborgaonkar007@gmail.com <gargeya.borgaonkar007@gmail.com>
2026-02-15 17:31:24 +0530

    15th Dec 2025

---
+++ src/test/java/com/testwithgargeya/ex02_Selenium_Basics/TestSelenium010Change_Driver.java
@@ -0,0 +1,10 @@
+package com.testwithgargeya.ex02_Selenium_Basics;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class TestSelenium010Change_Driver {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver = new ChromeDriver();
+    }
+}
